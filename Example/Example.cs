ea9b020fbed2ffd5f026ec98be1dd400ef11ce57
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-01-29 19:04:05 +0100

    Add core example project

---
+++ Example/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpFont;
+
+namespace Example
+{
+    class Example
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
